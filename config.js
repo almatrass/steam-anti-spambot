3fa7734ab80d004913d0b483452c6089c259b768
almatrass <rosscraigbrown@hotmail.co.uk>
2021-05-05 19:14:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   password: process.env.ACCOUNT_PASSWORD,
   sharedSecret: process.env.SHARED_SECRET,
   
-  bannedPhrases: [`Thanks for participating`, `Congratulations you won`, `we will send you your prize in 5 minutes`, `leave your link`, `Free CSGO Skins`, `no deposit`, `Only for new users`, `free SKIN CS:GO`],
+  bannedPhrases: [`Thanks for participating`, `Congratulations you won`, `we will send you your prize in 5 minutes`, `leave your link`, `Free CSGO Skins`, `no deposit`, `Only for new users`, `free SKIN CS:GO`, `Поздравляем`],
   publicUsername: `Anti-Spam BOT`,
   inGameName: `Anti-Spam BOT`
 };
